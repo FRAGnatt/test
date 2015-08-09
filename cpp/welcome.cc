b7bac1a08f72772d8b770215ab8c3ae8e3922397
Konstantin <FRAGnatt@gmail.com>
2015-08-09 14:38:15 +0300

    I can mixed 2 audio files BEEEEEEEEACH

--- cpp/welcome.cc
+++ cpp/welcome.cc
@@ -9,20 +9,36 @@ int main(int argc, char**argv) {
     // Prints welcome message...
     size_t size = 0; // here
     cout << "Welcome ..." << std::endl;
-    char djembeBuff[106121];
-    char metronomeBuff[106121];
+    char djembeBuff;
+    char metronomeBuff;
+    union {
+        char c[2];
+        short int si;
+    } djembeData;
+    union {
+        char c[2];
+        short int si;
+    } metronomeData;
+    union {
+        char c[2];
+        short int si;
+    } result;
+    
     ifstream djembe("C:/djembe-loCustom.wav", ios::binary);
     ifstream metronome("C:/metronomeupCustom.wav", ios::binary);
     ofstream newSound("C:/newsound.wav", ios::binary);    
     for (long i = 0; i < 40; i++) { // смещаем указатель до позиции с данными
-        djembe.get(djembeBuff[i]); // считали первое слово из файла            
-        metronome.get(metronomeBuff[i]); // считали первое слово из файла             
-        newSound << metronomeBuff[i];
+        djembe.read(&djembeBuff, 1); // считали первое слово из файла            
+        metronome.read(&metronomeBuff, 1); // считали первое слово из файла             
+        newSound << metronomeBuff;
     }
     for (long i = 40; i < (106120); i++) {
-        djembe.get(djembeBuff[i]); // считали первое слово из файла            
-        metronome.get(metronomeBuff[i]); // считали первое слово из файла             
-        newSound << metronomeBuff[i];
+        djembe.read(djembeData.c, 2); // считали первое слово из файла            
+        metronome.read(metronomeData.c, 2); // считали первое слово из файла     
+//        result.si = (djembeData.si * metronomeData.si) / 32768;
+        result.si = (djembeData.si + metronomeData.si) - ((djembeData.si * metronomeData.si) / 65536);
+        newSound << result.c[0];
+        newSound << result.c[1];
     }
     djembe.close(); // закрываем файл    
     metronome.close(); // закрываем файл  
